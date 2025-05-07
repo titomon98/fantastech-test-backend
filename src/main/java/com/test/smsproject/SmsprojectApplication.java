38a19380fd39ec8fdb1cf9834de60f717ed98729
titomon98 <arturomonterroso3@gmail.com>
2025-05-06 22:10:30 -0600

    Setting up the project

---
+++ src/main/java/com/test/smsproject/SmsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.test.smsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/smsproject/SmsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.smsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
